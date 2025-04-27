963193b5fc698cdfc2d5053c42bef7d30781a828
dinesh-more <moredinesh93@gmail.com>
2025-04-27 17:08:23 +0530

    added wishlist module

---
+++ e-commerce-frontend/src/app/wishlist/wishlist.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { WishlistService } from '../services/wishlist.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-wishlist',
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule],
+  templateUrl: './wishlist.component.html',
+  styleUrl: './wishlist.component.css'
+})
+export class WishlistComponent implements OnInit {
+  wishlistItems: any[] = [];
+  userId: any = null;
+
+  constructor(private wishlistService: WishlistService) {}
+
+  ngOnInit() {
+    this.loadWishlist();
+  }
+
+  loadWishlist() {
+    this.userId = localStorage.getItem('userId');
+    this.wishlistService.getWishlist(this.userId).subscribe(data => {
+      this.wishlistItems = data;
+      console.log('Wishlist items:', this.wishlistItems);
+    });
+  }
+
+  removeFromWishlist(productId: number) {
+    this.userId = localStorage.getItem('userId');
+    console.log('Removing product with ID:', productId);
+    this.wishlistService.removeProductFromWishlist(this.userId, productId).subscribe(() => {
+      this.loadWishlist();
+    });
+  }
+}
+
